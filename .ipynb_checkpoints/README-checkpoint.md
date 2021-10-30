4c402bc40d95e6de588c937e410c330195ae2c4c
lead-me-read-me <tmdgns1139@naver.com>
2021-10-30 12:09:57 +0000

    Add code for uploading cfg to WnB

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,8 @@
+# semantic-segmentation-level2-cv-18
+## from develop branch
+
+# 0. Quick Start
+```bash
+$ python test.py --cfg-yaml ./config/default.yaml
+```
+> By using mobilenet for 1 epoch, please check the pipeline.
--- config/wnb.py
+++ config/wnb.py
@@ -4,5 +4,7 @@ import wandb
 def wnb_init(cfg):
     exp_cfg = cfg["EXPERIMENTS"]
     if exp_cfg["WNB_TURN_ON"]:
-        return wandb.init(**exp_cfg["WNB_INIT"])
+        run = wandb.init(**exp_cfg["WNB_INIT"])
+        wandb.config.update(cfg)
+        return run
     
